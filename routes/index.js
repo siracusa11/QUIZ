9bd8212c18a9d1bf5a310af6b11140059f378ff5
siracusa11 <lopezalvaro006@gmail.com>
2015-04-27 19:15:57 +0200

    QUIZ para Heroku

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'QUIZ' });
 });
+
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
 
 router.get('/authors', function(req, res, next) {
   res.render('authors', { title: 'AUTORES' });
