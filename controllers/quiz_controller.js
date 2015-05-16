04be8ef31493d0d8bcacb7ce70037f3ee17fbba8
mafer6 <marta.ferrerasma@alumnos.upm.es>
2015-05-16 13:17:02 +0200

    Autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,23 +2,37 @@
 
 var models = require('../models/models.js');
 
+// Autoload - factoriza el código si la ruta incluye :quizId
+exports.load = function(req, res, next, quizId) {
+	models.Quiz.find(quizId).then(
+		function(quiz) {
+			if (quiz) {
+				req.quiz = quiz;
+				next();
+			} else {
+				next(new Error('No existe quizId=' + quizId));
+			}
+		}
+	).catch(function(error) {next(error);});
+}
 
-//GET /quizes/:id
+
+//GET /quizes/:id -> Solo si existe el id llega aquí
 exports.show = function(req, res) {
 	models.Quiz.find(req.params.quizId).then(function(quiz) {
-		res.render('quizes/show', {quiz: quiz});
+		res.render('quizes/show', {quiz: req.quiz}); 
 	})
 };
 
-//GET /quizes/:id/answer
+//GET /quizes/:id/answer -> Solo si existe el id llega aquí
 exports.answer = function(req, res) {
 	models.Quiz.find(req.params.quizId).then(function(quiz) {
-		if (req.query.respuesta === quiz.respuesta){
+		if (req.query.respuesta === req.quiz.respuesta){
 			res.render('quizes/answer', 
-				{ quiz:quiz, respuesta: 'Correcto. Eres un Mewtwo.'});
+				{ quiz:req.quiz, respuesta: 'Correcto. Eres un Mewtwo.'});
 		} else {
 			res.render('quizes/answer', 
-				{ quiz:quiz, respuesta: 'Incorrecto. Eres un Magikarp.'});
+				{ quiz:req.quiz, respuesta: 'Incorrecto. Eres un Magikarp.'});
 		}
 	})
 };
@@ -31,12 +45,13 @@ exports.index = function(req, res) {
 	models.Quiz.findAll({where: ["pregunta like ?", search_like],
 			 			 order: [['updatedAt', 'DESC']]
 						})
-	.then(function(quizes) {
-		res.render('quizes/index.ejs', {
-			quizes: quizes
-		});
-	})
-}
+	.then(
+		function(quizes) {
+			res.render('quizes/index.ejs', { quizes: quizes });
+		}
+	//Si error, pasa al middleware de error
+	).catch(function(error) {next(error);}) 
+};
 
 
 //Cambio success por then por problemas de versiones
--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'QUIZ' });
 });
 
+// Autoload de comandos con :quizId  
+router.param('quizId', quizController.load); //autoload solo se invoca si lleva :quizId
+
 // GET /quizes?search = "Texto" --> ? indica parámetro opcional
 router.get('/quizes:search?', quizController.index);
-router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)', quizController.show); //No solo valores numéricos
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 router.get('/authors', function(req, res, next) {
