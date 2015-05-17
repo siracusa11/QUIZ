6ede0b4ee0d8567a9f0f8a4e873d2ebad08e8f5e
mafer6 <marta.ferrerasma@alumnos.upm.es>
2015-05-17 04:35:09 +0200

    Validación y sesión

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET paǵina de entrada (home page) . */
 router.get('/', function(req, res, next) {
@@ -11,6 +12,11 @@ router.get('/', function(req, res, next) {
 
 // Autoload de comandos con :quizId  
 router.param('quizId', quizController.load); //autoload solo se invoca si lleva :quizId
+
+// Definición de rutas de sesión
+router.get('/login', sessionController.new); 		// formulario login
+router.post('/login', sessionController.create); 	//crear sesión
+router.get('/logout', sessionController.destroy);	//destruir sesión
 
 // Definición de rutas de /quizes
 router.get('/quizes:search?', quizController.index); // GET /quizes?search = "Texto" --> ? indica parámetro opcional
@@ -22,6 +28,7 @@ router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
 router.put('/quizes/:quizId(\\d+)', quizController.update);
 router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
 
+// Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new); //Accede al formulario de crear comentario
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create); //Crea una entrada en la tabla de comentarios
 
